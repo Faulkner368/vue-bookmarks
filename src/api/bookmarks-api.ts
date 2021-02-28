06d8c416686d7b9e7ee8ebdd64658ff3b2fc271f
Faulkner368 <61080745+Faulkner368@users.noreply.github.com>
2021-02-28 15:34:35 +0000

    cant remember why made changes

--- src/api/bookmarks-api.ts
+++ src/api/bookmarks-api.ts
@@ -450,7 +450,181 @@ export class Api {
         return Promise.resolve<Tag[]>(<any>null);
     }
 
-    // path('tag/search/<str:tag_title>', views.tag_search, name='tag_search'),
+    /**
+     * Returns tags containing the search term, case insensitive
+     * @param tagTitle
+     * @return Tag[]
+     */
+    tagSearchByTitle(searchTerm: string): Promise<Tag[]> {
+        let url_ = this.baseUrl + "tag/search/{searchTerm}";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": this.apiKey
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processTagSearchByTitle(_response);
+        });
+    }
+
+    protected processTagSearchByTitle(response: Response): Promise<Tag[]> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            if (Array.isArray(resultData200)) {
+                result200 = [] as any;
+                for (let item of resultData200)
+                    result200!.push(Tag.fromJS(item));
+            }
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<Tag[]>(<any>null);
+    }
+
+    /**
+     * Adds the given tag object to list of tags
+     * @param tag
+     * @return Tag
+     */
+    addTag(tag: Tag): Promise<Tag> {
+        let url_ = this.baseUrl + "tag/add";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": this.apiKey
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processAddTag(_response);
+        });
+    }
+
+    protected processAddTag(response: Response): Promise<Tag> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            if (Array.isArray(resultData200)) {
+                result200 = [] as any;
+                for (let item of resultData200)
+                    result200!.push(Tag.fromJS(item));
+            }
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<Tag>(<any>null);
+    }
+
+    /**
+     * Updates given tag using the tags ID
+     * @param tag
+     * @return Tag
+     */
+    updateTag(tag: Tag): Promise<Tag> {
+        let url_ = this.baseUrl + "tag/update/{tag.id}";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "PUT",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": this.apiKey
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processUpdateTag(_response);
+        });
+    }
+
+    protected processUpdateTag(response: Response): Promise<Tag> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            if (Array.isArray(resultData200)) {
+                result200 = [] as any;
+                for (let item of resultData200)
+                    result200!.push(Tag.fromJS(item));
+            }
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<Tag>(<any>null);
+    }
+
+    /**
+     * Deletes given tag by its ID and returns ID to confirm
+     * @param tag
+     * @return Tag
+     */
+    deleteTag(tag: Tag): Promise<IApiResponse> {
+        let url_ = this.baseUrl + "tag/delete/{tag.id}";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ = <RequestInit>{
+            method: "DELETE",
+            headers: {
+                "Accept": "application/json",
+                "Authorization": this.apiKey
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processDeleteTag(_response);
+        });
+    }
+
+    protected processDeleteTag(response: Response): Promise<IApiResponse> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            if (Array.isArray(resultData200)) {
+                result200 = [] as any;
+                for (let item of resultData200)
+                    result200!.push(Tag.fromJS(item));
+            }
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<IApiResponse>(<any>null);
+    }
     // path('tag/add', views.add_tag, name="add_tag"),
     // path('tag/update/<str:tag_id>', views.update_tag_by_id, name="update_tag_by_id"),
     // path('tag/delete/<str:tag_id>', views.delete_tag_by_id, name="delete_tag_by_id")
